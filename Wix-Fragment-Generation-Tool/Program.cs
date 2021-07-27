567504d10e0950ebcd6506ab1bc876ac97a32f3c
SergeiTereshenko <stereshenko@mirantis.com>
2021-07-27 07:08:51 -0700

    create project

---
+++ Wix-Fragment-Generation-Tool/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wix_Fragment_Generation_Tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hw");
+            Console.ReadLine();
+            
+        }
+    }
+}
